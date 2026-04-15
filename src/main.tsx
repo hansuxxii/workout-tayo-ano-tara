5aa640a808fe73705cd50218fcf0f0ab3ce0f0d1
hansuxxii <167149682+hansuxxii@users.noreply.github.com>
2026-04-15 09:18:50 +0800

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WorkoutTayoAnoTaraApp from "./WorkoutTayoApp";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <WorkoutTayoAnoTaraApp />
+  </React.StrictMode>
+);
